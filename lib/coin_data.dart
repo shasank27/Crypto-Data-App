5955a768dc8d663fdc319fd144c9827c1d968d62
shasank27 <shasankperiwal2027@gmail.com>
2021-05-23 14:42:17 +0530

    Crypto Data App

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,18 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Map<String, String> prices = {};
+  Future getMoney(String selectedcurrency) async {
+    for (String i in cryptoList) {
+      http.Response response = await http.get(Uri.parse(
+          'https://rest.coinapi.io/v1/exchangerate/$i/$selectedcurrency?apikey=93100797-DED9-4718-A126-2ED5496560A8'));
+      var decoded = await jsonDecode(response.body);
+      print(decoded['rate']);
+      if (decoded['rate'] != null) {
+        prices[i] = decoded['rate'].toStringAsFixed(0);
+      }
+    }
+    return prices;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +7,66 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  Map<String, String> coinprice;
+  String selectedcurrency = 'USD';
+  bool gotPrice = false;
+  CoinData coin = CoinData();
+  List<DropdownMenuItem> dropdown() {
+    List<DropdownMenuItem> dropdownMenuItem = [];
+    for (String i in currenciesList) {
+      var newitem = DropdownMenuItem(
+        child: Text(i),
+        value: i,
+      );
+      dropdownMenuItem.add(newitem);
+    }
+    return dropdownMenuItem;
+  }
+
+  getPrice() async {
+    gotPrice = false;
+    try {
+      var data = await coin.getMoney(selectedcurrency);
+      gotPrice = true;
+      setState(() {
+        coinprice = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Padding pad(String crypto) {
+    String price = gotPrice ? coinprice[crypto] : '?';
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $crypto = $price $selectedcurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    getPrice();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,33 +77,24 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          pad(cryptoList[0]),
+          pad(cryptoList[1]),
+          pad(cryptoList[2]),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton(
+              value: selectedcurrency,
+              items: dropdown(),
+              onChanged: (value) {
+                setState(() {
+                  selectedcurrency = value;
+                  getPrice();
+                });
+              },
+            ),
           ),
         ],
       ),
